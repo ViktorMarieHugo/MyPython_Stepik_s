1f011fa86a62e6fc8f109a27fab38def9f57ac40
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-03 17:18:22 +0300

    refactor: code

--- PasswordGenerator/pass_gen.py
+++ PasswordGenerator/pass_gen.py
@@ -5,8 +5,25 @@ inclulow = 'abcdefghijklmnopqrstuvwxyz'
 iclucaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 inclusimbols = '!#$%&*+-=?@^_'
 ambiguous = 'il1Lo0O'
+#count_of_pass = int(input("dqdqwdqwd"))
+while True:
+    length = int(input("Enter the desired password length in the range from 1  \n"))
+
+    if length>=1:
+        break
+    else:
+        True
+
 chars = user_request(inclunums,iclucaps,inclulow,inclusimbols,ambiguous)
-length = int(input())
-def generate_password(length, char):
+
+def generate_password(length, chars):
     '''генерирует и возвращает пароль. принимает длину пароля и chars (сoстав)'''
-    
+    password = []
+    for _ in range(int(length)):
+        k = choice(chars)
+        password.append(k)
+    print(*password, sep = '')
+generate_password(length, chars)
+
+#for i in range(count_of_pass):
+#    generate_password(length, chars)
