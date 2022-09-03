94b38c7e52e4b5c1796768067315c17d2318d890
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-03 17:21:18 +0300

    refactor: code

--- PasswordGenerator/user_request.py
+++ PasswordGenerator/user_request.py
@@ -2,28 +2,28 @@ def user_request(inclunums,iclucaps,inclulow,inclusimbols,ambiguous):
     '''собирает от пользователя информацию о необходимом содержании пароля
     все переменные в теле программы'''
     chars = []
-    inclunums_q = input("Whether to include numbers?  Yes/No")
+    inclunums_q = input("Whether to include numbers?  Yes/No \n")
     if inclunums_q.lower() == "yes":
         chars.append(inclunums)
     elif inclunums_q.lower() == "no":
         pass
     else:
         pass
-    iclucaps_q = input("Whether to include uppercase letters? Yes/No")
+    iclucaps_q = input("Whether to include uppercase letters? Yes/No \n")
     if iclucaps_q.lower() == "yes":
         chars.append(iclucaps)
     elif iclucaps_q.lower() == "no":
         pass
     else:
         pass
-    inclulow_q = input("Whether to include lowercase letters? Yes/No")
+    inclulow_q = input("Whether to include lowercase letters? Yes/No \n")
     if inclulow_q.lower() == "yes":
          chars.append(inclulow)
     elif inclulow_q.lower() == "no":
         pass
     else:
         pass
-    inclusimbols_q = input("Whether to include symbols? Yes/No")
+    inclusimbols_q = input("Whether to include symbols? Yes/No \n")
     if inclusimbols_q.lower() == "yes":
         chars.append(inclusimbols)
     elif inclusimbols_q.lower() == "no":
@@ -32,7 +32,7 @@ def user_request(inclunums,iclucaps,inclulow,inclusimbols,ambiguous):
         pass
     chars = ''.join(chars)
     chars1 = []
-    exclude_ambiguous = input("Whether to exclude ambiguous characters Yes/No")
+    exclude_ambiguous = input("Whether to exclude ambiguous characters Yes/No \n")
     if exclude_ambiguous.lower() == "yes":
         for i in range(len(chars)):
             if chars[i] not in ambiguous:
