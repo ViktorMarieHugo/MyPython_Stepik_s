54b3f4936715b4321526c18211db37c26cd06b68
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-04 10:59:03 +0300

    refactor: code

--- PasswordGenerator/pass_gen.py
+++ PasswordGenerator/pass_gen.py
@@ -8,25 +8,18 @@ inclusimbols = '!#$%&*+-=?@^_'
 ambiguous = 'il1Lo0O'
 while True: # проверка, что количесво паролей хотя бы 1
     count_of_pass = int(input("How many passwords would you like to generate? \n You need to select at least 1 \n"))
-
     if count_of_pass>=1:
         break
     else:
         True
-
 while True: # проверка, что количесво символов не равно 0
     length = int(input("Enter the desired password length  \n"))
-
     if length>=1:
         break
     else:
         True
-
 chars = user_request(inclunums,iclucaps,inclulow,inclusimbols,ambiguous)
-
 print("\n Your password(s) is: \n")
-
 for i in range(count_of_pass):
-
     print(*generate_password(length, chars), sep='')
     print()
