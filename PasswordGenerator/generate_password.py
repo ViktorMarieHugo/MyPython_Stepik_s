19dc5652b9bbf1a819c9b7f845bc8a2fe75172cb
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-04 10:31:11 +0300

    refactor: DEF and code

---
+++ PasswordGenerator/generate_password.py
@@ -0,0 +1,7 @@
+def generate_password(length, chars):
+    '''генерирует и возвращает пароль. принимает длину пароля и chars (сoстав)'''
+    password = []
+    for _ in range(int(length)):
+        k = choice(chars)
+        password.append(k)
+    return password
