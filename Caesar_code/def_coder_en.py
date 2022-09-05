ae11be4d7a46adde4bbbcae0703f13e7172ef037
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-05 14:21:41 +0300

    new code

--- Caesar_code/def_coder_en.py
+++ Caesar_code/def_coder_en.py
@@ -1,17 +1,30 @@
-key = int(input())
-text = str(input())
-symbols = ' .,!?;:-'
-new_text = ''
-for i in range(len(text)):
-	
-	if text[i]in symbols:
-		new_text += text[i]
-	else:
-		num = ord(text[i])
-		new = num - key
-		if new < 97:
-			new = chr(num - key + 26)
-		else:
-			new = chr(num - key)
-			new_text += new
-print(new_text)  
+def coder_en(key,text):
+    nums = '0123456789'
+    symbols = ' .,!?;:-'
+    en = 'abcdefghijklmnopqrstuvwxyz'
+    EN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for i in range(len(text)):
+        if text[i]in symbols or text[i] in nums:
+            new_text += text[i]
+            
+        if text[i] in EN:
+            num = ord(text[i])
+            new = num - key
+            if new < 65:
+                new = chr(num - key + 26)
+                new_text += new
+            else:
+                new = chr(num - key)
+                new_text += new
+
+        if text[i] in en:
+            num = ord(text[i])
+            new = num - key
+            if new < 97:
+                new = chr(num - key + 26)
+                new_text += new
+            else:
+                new = chr(num - key)
+                new_text += new
+    print(new_text)  
