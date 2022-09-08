60a73cb8119d7dc095049b6d9389aac47633c2bc
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-08 09:20:57 +0300

    fix: code

--- Caesar_code/def_coder_ru.py
+++ Caesar_code/def_coder_ru.py
@@ -1,21 +1,18 @@
-def coder_ru(key,text):
+def coder_ru(key, text):
     nums = '0123456789'
     symbols = ' .,!?;:-'
     ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
     RU = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     new_text = ''
-    for i in range(len(text)):
-        if text[i]in symbols or text[i] in nums:
-            new_text += text[i]            
-        if text[i] in RU:
-            num = RU.find(text[[i]])
-            new = num - key
+    for i in text:
+        if i in symbols or i in nums:
+            new_text += i            
+        if i in RU:
+            num = RU.find(i)
+            new = num + key
             new_text += RU[new]         
-        if text[i] in ru:
-            num = ru.find(text[[i]])
-            new = num - key
+        if i in ru:
+            num = ru.find(i)
+            new = num + key
             new_text += ru[new]
-    return new_text  
-
-
-print(coder_en(25,'Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd.'))
+    return new_text
