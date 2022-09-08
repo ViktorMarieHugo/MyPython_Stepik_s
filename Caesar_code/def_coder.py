0965184059235b53354b341f75b7731cd604d26f
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-08 20:56:41 +0300

    upd: code

--- Caesar_code/def_coder.py
+++ Caesar_code/def_coder.py
@@ -25,31 +25,83 @@ def coder_ru(key, text):
 				new_text += ru[new]
 	return new_text
 
+def decoder_ru(key, text):
+	nums = '0123456789'
+	symbols = ' .,!?;:-'
+	ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+	RU = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+	new_text = ''
+	for i in text:
+		if i in symbols or i in nums:
+			new_text += i            
+		if i in RU:
+			num = RU.find(i)
+			new = num - key
+			if new < 32:
+				new = num - key + 32
+				new_text += RU[new]
+			else:
+				new_text += RU[new]         
+		if i in ru:
+			num = ru.find(i)
+			new = num - key
+			if new < 32:
+				new = num - key + 32
+				new_text += ru[new]
+			else:
+				new_text += ru[new]
+	return new_text
+
 def coder_en(key,text):
     nums = '0123456789'
     symbols = ' .,!?;:-'
     en = 'abcdefghijklmnopqrstuvwxyz'
     EN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
-    for i in range(len(text)):
-        if text[i]in symbols or text[i] in nums:
-            new_text += text[i]            
-        if text[i] in EN:
-            num = ord(text[i])
+    for i in text:
+        if i in symbols or i in nums:
+            new_text += i            
+        if i in EN:
+            num = EN.find(i)
             new = num - key
-            if new > 65:
-                new = chr(num + key - 26)
-                new_text += new
+            if new < 26:
+                new = num - key + 26
+                new_text += EN[new]
             else:
-                new = chr(num + key)
-                new_text += new
-        if text[i] in en:
-            num = ord(text[i])
+                new_text += EN[new]         
+        if i in en:
+            num = en.find(i)
             new = num - key
-            if new > 97:
-                new = chr(num + key - 26)
-                new_text += new
+            if new < 26:
+                new = num - key + 26
+                new_text += en[new]
             else:
-                new = chr(num + key)
-                new_text += new
+                new_text += en[new]
     return new_text  
+
+def decoder_en(key,text):
+    nums = '0123456789'
+    symbols = ' .,!?;:-'
+    en = 'abcdefghijklmnopqrstuvwxyz'
+    EN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for i in text:
+        if i in symbols or i in nums:
+            new_text += i            
+        if i in EN:
+            num = EN.find(i)
+            new = num - key
+            if new < 26:
+                new = num - key + 26
+                new_text += EN[new]
+            else:
+                new_text += EN[new]         
+        if i in en:
+            num = en.find(i)
+            new = num - key
+            if new < 26:
+                new = num - key + 26
+                new_text += en[new]
+            else:
+                new_text += en[new]
+    return new_text
--- Caesar_code/сaesar_shift.py
+++ Caesar_code/сaesar_shift.py
@@ -12,3 +12,13 @@ if direction == 'e':
         text = input("enter your text\n")
         key = int(input("Which shift step will we use?(1-26)\n"))
         print(coder_en(key, text))
+if direction == 'd':
+    language = input("What is the alphabet language: Russian (R) or English (E)\n").lower()
+    if language == 'r':
+        text = input("enter your text\n")
+        key = int(input("Which shift step will we use?(1-32)\n"))
+        print(decoder_ru(key, text))
+    if language == 'e':
+        text = input("enter your text\n")
+        key = int(input("Which shift step will we use?(1-26)\n"))
+        print(decoder_en(key, text))
