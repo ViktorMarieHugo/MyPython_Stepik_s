4d27fb339be19ad059c40945d11d6482cee32e8a
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-09 10:29:59 +0300

    upd: recode

--- Caesar_code/def_coder.py
+++ Caesar_code/def_coder.py
@@ -26,31 +26,31 @@ def coder_ru(key, text):
 	return new_text
 
 def decoder_ru(key, text):
-	nums = '0123456789'
-	symbols = ' .,!?;:-'
-	ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
-	RU = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-	new_text = ''
-	for i in text:
-		if i in symbols or i in nums:
-			new_text += i            
-		if i in RU:
-			num = RU.find(i)
-			new = num - key
-			if new < 32:
-				new = num - key + 32
-				new_text += RU[new]
-			else:
-				new_text += RU[new]         
-		if i in ru:
-			num = ru.find(i)
-			new = num - key
-			if new < 32:
-				new = num - key + 32
-				new_text += ru[new]
-			else:
-				new_text += ru[new]
-	return new_text
+    nums = '0123456789'
+    symbols = ' .,!?;:-'
+    ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    RU = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    new_text = ''
+    for i in text:
+        if i in symbols or i in nums:
+            new_text += i            
+        if i in RU:
+            num = RU.find(i)
+            new = num - key
+            if new < 0:
+                new = num - key + 32
+                new_text += RU[new]
+            else:
+                new_text += RU[new]         
+        if i in ru:
+            num = ru.find(i)
+            new = num - key
+            if new < 0:
+                new = num - key + 32
+                new_text += ru[new]
+            else:
+                new_text += ru[new]
+    return new_text
 
 def coder_en(key,text):
     nums = '0123456789'
@@ -63,21 +63,21 @@ def coder_en(key,text):
             new_text += i            
         if i in EN:
             num = EN.find(i)
-            new = num - key
-            if new < 26:
-                new = num - key + 26
+            new = num + key
+            if new > 26:
+                new = num + key - 26
                 new_text += EN[new]
             else:
                 new_text += EN[new]         
         if i in en:
             num = en.find(i)
-            new = num - key
-            if new < 26:
-                new = num - key + 26
+            new = num + key
+            if new > 26:
+                new = num + key - 26
                 new_text += en[new]
             else:
                 new_text += en[new]
-    return new_text  
+    return new_text
 
 def decoder_en(key,text):
     nums = '0123456789'
