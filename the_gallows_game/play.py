2bc846c7c34b08db07244b128fbc4a802056848c
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-13 11:53:55 +0300

    upd: code

--- the_gallows_game/play.py
+++ the_gallows_game/play.py
@@ -1,14 +1,9 @@
 from def_get_word import get_word
 from def_display_hangman import display_hangman
 RU = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-def key():
-    guessed = input("Сыграете ещё?  Да\Нет").lower()
-    if guessed == 'нет':
-        guessed = False
-    else:
-        guessed = True
+
 def play():
-    guessed = False
+    guessed = True
     print("Добро пожаловать в игру 'Виселица'!")
     word = get_word()
     print(word)
@@ -21,7 +16,7 @@ def play():
     user_guess = list(word_completion)
     
 
-    while True:
+    while guessed == True:
         user_input = input("Введите букву или слово целиком\n").upper() 
         if len(user_input) == len(word):
             if user_input == word:
@@ -32,8 +27,13 @@ def play():
                     tries -= 1
                     print("Это была неверная попытка", display_hangman(tries))
                 else:
-                    print("Поздравляем, Вы отгадали слово")
-                    break
+                    print("Поздравляем, Вы отгадали слово!")
+                    guessed = input("Сыграете ещё?  Да\Нет\n").lower()
+                    if guessed == 'нет':
+                        break
+                    if guessed == 'да':
+                        play()
+                    
             else:
                 tries -= 1
                 unguessed_words.append(user_input)
@@ -50,8 +50,12 @@ def play():
                 print("\nВерррно!!!  Есть такая Буква!\n")        
                 print(*user_guess)
                 if ''.join(user_guess) == word:
-                    print("Поздравляем, Вы отгадали слово")
-                    break
+                    print("Поздравляем, Вы отгадали слово!")
+                    guessed = input("Сыграете ещё?  Да\Нет\n").lower()
+                    if guessed == 'нет':
+                        break
+                    if guessed == 'да':
+                        play()
                 else:
                     pass            
             else:
