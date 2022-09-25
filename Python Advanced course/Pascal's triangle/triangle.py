c53c94de4fa81c0f0f006a0c675f0585f352438d
ViktorMarieHugo <sektorshans86@gmail.com>
2022-09-25 11:39:47 +0300

    upd: code

--- Python Advanced course/Pascal's triangle/triangle.py
+++ Python Advanced course/Pascal's triangle/triangle.py
@@ -1,4 +1,4 @@
-n = int(input())
+n = int(input())    
 pascal = []
 for i in range(n + 1):
     pascal.append([1] + [0]*n)
@@ -6,4 +6,9 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1]
 for i in range(0, n+1):
-    print(pascal[i])
+    print(pascal[i])                    #Вывод в виде таблицы с квадратными скобками
+print()
+for i in range(0, n+1):
+    for j in range(0, n+1):         
+        print(pascal[i][j], end = ' ')  #Вывод в виде таблицы без скобок
+    print()
