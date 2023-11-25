d7c30b496fe6f24ad8fb7ac96a626d37be107719
ViktorMarieHugo <sektorshans86@gmail.com>
2023-11-25 08:05:54 -0800

    Повторение пройденного материала 2023  On branch master  Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   Caesar_code/__pycache__/def_coder.cpython-311.pyc
    	modified:   "Caesar_code/\321\201aesar_shift.py"
    	new file:   Magicball_8/__pycache__/user_question.cpython-311.pyc
    	new file:   My FIRST PROGR AGAIN 2023/Hello World
    	new file:   My FIRST PROGR AGAIN 2023/Print
    	new file:   My FIRST PROGR AGAIN 2023/nums
    	new file:   My FIRST PROGR AGAIN 2023/print a b c
    	new file:   My FIRST PROGR AGAIN 2023/sep & ends

--- Caesar_code/сaesar_shift.py
+++ Caesar_code/сaesar_shift.py
@@ -1,24 +1,30 @@
 from def_coder import *
 
 print("Welcome to the Caesar Cipher!\n")
-direction = input("Which direction will we use? Encryption(E) or decryption?(D)\n").lower()
-if direction == 'e':
-    language = input("What is the alphabet language: Russian (R) or English (E)\n").lower()
-    if language == 'r':
+direction = input(
+    "Which direction will we use? Encryption(E) or decryption?(D)\n"
+).lower()
+if direction == "e":
+    language = input(
+        "What is the alphabet language: Russian (R) or English (E)\n"
+    ).lower()
+    if language == "r":
         text = input("enter your text\n")
         key = int(input("Which shift step will we use?(1-32)\n"))
         print(coder_ru(key, text))
-    if language == 'e':
+    if language == "e":
         text = input("enter your text\n")
         key = int(input("Which shift step will we use?(1-26)\n"))
         print(coder_en(key, text))
-if direction == 'd':
-    language = input("What is the alphabet language: Russian (R) or English (E)\n").lower()
-    if language == 'r':
+if direction == "d":
+    language = input(
+        "What is the alphabet language: Russian (R) or English (E)\n"
+    ).lower()
+    if language == "r":
         text = input("enter your text\n")
         key = int(input("Which shift step will we use?(1-32)\n"))
         print(decoder_ru(key, text))
-    if language == 'e':
+    if language == "e":
         text = input("enter your text\n")
         key = int(input("Which shift step will we use?(1-26)\n"))
         print(decoder_en(key, text))
